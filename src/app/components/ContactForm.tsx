5161e025c0ab36f5a4a57323e589862744d7f6d5
MIGUEL MORT <devmigueluicab@gmail.com>
2024-12-24 04:30:46 -0600

    Update ContactForm.tsx

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -1,17 +1,51 @@
 'use client'
-
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Button } from "@nextui-org/button"
-import { Input } from "@nextui-org/input"
-import { Textarea } from "@nextui-org/input"
+import { Input, Textarea } from "@nextui-org/input"
 
 export function ContactForm() {
     const [isLoading, setIsLoading] = useState(false)
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
 
     async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
         setIsLoading(true)
+        try {
+            const response = await fetch(`https://api.kodikas.test/api/contact-me`, {
+                method: 'POST',
+                body: JSON.stringify(formData),
+            })
 
+            if (response.ok) {
+                alert({
+                    title: "Mensaje enviado",
+                    description: "Nos pondremos en contacto contigo pronto.",
+                });
+                setFormData({ name: '', email: '', message: '' });
+            } else {
+                throw new Error('Error al enviar el mensaje');
+            }
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                alert('ERROR: ' + error.message);
+            } else {
+                console.error('Unknown error:', error);
+            }
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -19,26 +53,35 @@ export function ContactForm() {
             <div className="space-y-2">
                 <Input
                     id="name"
+                    name="name"
                     placeholder="Nombre completo"
                     required
                     className="bg-zinc-900"
+                    value={formData.name}
+                    onChange={handleChange}
                 />
             </div>
             <div className="space-y-2">
                 <Input
                     id="email"
+                    name="email"
                     placeholder="Email"
                     required
                     type="email"
                     className="bg-zinc-900"
+                    value={formData.email}
+                    onChange={handleChange}
                 />
             </div>
             <div className="space-y-2">
                 <Textarea
                     id="message"
+                    name="message"
                     placeholder="Mensaje"
                     required
                     className="min-h-[150px] bg-zinc-900"
+                    value={formData.message}
+                    onChange={handleChange}
                 />
             </div>
             <Button
@@ -51,4 +94,3 @@ export function ContactForm() {
         </form>
     )
 }
-
