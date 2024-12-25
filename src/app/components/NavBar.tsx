fbd21d116ee0d7d9f5d2be776198ab3cda09839d
MIGUEL MORT <devmigueluicab@gmail.com>
2024-12-25 15:42:09 -0600

    Update NavBar.tsx

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -34,7 +34,7 @@ export function NavBar() {
     }, []);
 
     return (
-        <nav className="fixed top-0 z-50 w-full">
+        <nav className="fixed top-0 z-50 w-full bg-black/75">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
