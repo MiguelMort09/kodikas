3004292e1cea4b8ba3493b3581209cfd15083b47
MIGUEL MORT <devmigueluicab@gmail.com>
2024-12-25 18:49:49 -0600

    Correction typing

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,10 +1,16 @@
-import { ImageResponse } from 'next/og'
-import { join } from 'node:path'
-import { readFile } from 'node:fs/promises'
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { ImageResponse } from '@vercel/og';
 
-export default async function Image() {
-    const logoData = await readFile(join(process.cwd(), 'Kodikas_IMAGO_CLARO.png'))
-    const logoSrc = Uint8Array.from(logoData).buffer
+export const config = {
+    runtime: 'edge', // Usa la función como un middleware en el edge
+};
+
+export default async function OpengraphImage() {
+    const logoPath = join(process.cwd(), 'Kodikas_IMAGO_CLARO.png');
+    const logoData = await readFile(logoPath); // Lee el archivo
+    const logoBase64 = logoData.toString('base64'); // Convierte la imagen a Base64
+    const logoSrc = `data:image/png;base64,${logoBase64}`; // Define el formato de la imagen
 
     return new ImageResponse(
         (
@@ -13,10 +19,24 @@ export default async function Image() {
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: '#fff', // Fondo blanco
                 }}
             >
-                <img src={logoSrc} height="100" />
+                <img
+                    src={logoSrc}
+                    alt="KODIKAS"
+                    style={{
+                        width: '200px',
+                        height: '200px', // Ajusta el tamaño de la imagen según sea necesario
+                    }}
+                />
             </div>
-        )
-    )
+        ),
+        {
+            width: 1200, // Ancho de la imagen OG
+            height: 630, // Alto de la imagen OG
+        }
+    );
 }
